c60da4bbca0c1ae7165ed2a786501e5a0d6bf263
Joshua Martin <the.manuscriptmaster@gmail.com>
2018-12-26 15:50:12 -0500

    Reset app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodoList } from './todo-list/todo-list.component';
-import { TodoListItem } from './todo-list-item/todo-list-item.component';
-import { TodoService } from './services';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoList,
-    TodoListItem
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
-  providers: [TodoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
